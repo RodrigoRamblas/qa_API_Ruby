fb2d8600f549a292ee1b062a985326db0b94d037
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2021-01-25 12:06:00 -0300

    Added the zipCode.rb file and the BDD scenarios

---
+++ features/step_definitions/zipCode.rb
@@ -0,0 +1,19 @@
+Given("that I have a valid zip code") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I make a request") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I must know if this zip code is valid") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("that I have a invalid zip code") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I must know if this zip code is invalid") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
